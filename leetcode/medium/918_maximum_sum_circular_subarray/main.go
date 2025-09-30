0b3e4d7fbc368cd6f9841e54a453fb74d4c80ba7
liubavabarsegian <lyubahemmo@mail.ru>
2025-09-30 16:48:56 +0300

    918 still in progress

--- leetcode/medium/918_maximum_sum_circular_subarray/main.go
+++ leetcode/medium/918_maximum_sum_circular_subarray/main.go
@@ -10,61 +10,25 @@ func sum(nums []int) int {
 	return sum
 }
 
-// func maxSubarraySumCircular(nums []int) int {
-// 	circle := append(nums, nums...)
-// 	totalSum := circle[0]
+func minSubarraySum(nums []int) int {
+	minSum := nums[0]
+	currentMinSum := minSum
 
-// 	currentSum := sum(circle)
-// 	for len(circle) > 0 {
-// 		if circle[0] > circle[len(circle)-1] {
-// 			currentSum -= circle[len(circle)-1]
-// 			circle = circle[:len(circle)-1]
-// 		} else {
-// 			currentSum -= circle[0]
-// 			circle = circle[1:]
-
-// 		}
-// 		if len(circle) <= len(nums) {
-// 			totalSum = max(totalSum, currentSum)
-// 		}
-// 		fmt.Println(circle, totalSum, currentSum)
-// 	}
-// 	return totalSum
-// }
-
-func maxSubarraySumCircular(nums []int) int {
-	circle := append(nums, nums...)
-
-	maxSubarray := circle[0]
-	newMax := circle[0]
-	subArrayLen := 0
-
-	for i := 1; i < len(circle); i++ {
-		fmt.Println(maxSubarray, newMax, circle)
-		offset := i % len(circle)
-		// если число положительное, то оно увеличит сумму, поэтому его берем
-		for subArrayLen >= len(nums)-1 || offset > subArrayLen-1 && newMax-circle[offset-subArrayLen] < newMax {
-
-			newMax -= circle[offset-subArrayLen]
-			fmt.Println("new max", newMax, circle[offset-subArrayLen+1], offset)
-			subArrayLen--
+	for i := 1; i < len(nums); i++ {
+		currentMinSum += nums[i]
+		if nums[i] < currentMinSum {
+			currentMinSum = nums[i]
 		}
-		if newMax < 0 || offset > 0 && circle[offset-1] < 0 && newMax+circle[offset] < newMax {
-			// сброс подмассива
-			fmt.Println("reset", newMax, circle[offset])
-			newMax = circle[offset]
-			subArrayLen = 0
-		} else {
-			newMax += circle[offset]
-			subArrayLen++
-			fmt.Println("add", newMax, circle[offset])
-		}
-
-		maxSubarray = max(maxSubarray, newMax)
+		minSum = min(minSum, currentMinSum)
 	}
-	return maxSubarray
+	return minSum
 }
 
+func maxSubarraySumCircular(nums []int) int {
+	return sum(nums) - minSubarraySum(nums)
+}
+
+// 1 -2 3 2
 func main() {
 	fmt.Println(maxSubarraySumCircular([]int{1, -2, 3, -2})) // 3
 	fmt.Println(maxSubarraySumCircular([]int{5, -3, 5}))     // 10
